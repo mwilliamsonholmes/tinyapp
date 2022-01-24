3f1d2a1809d82d1b3389aedd28e46ab3d6d46a15
Melissa <mawilliamson10@gmail.com>
2022-01-24 23:42:25 +0000

    added new route to render new urls_show template

--- express_server.js
+++ express_server.js
@@ -13,6 +13,19 @@ app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
+
+app.get("/urls/:shortURL", (req, res) => {
+  const templateVars = {
+    shortURL: req.params.shortURL,
+    longURL: urlDatabase[req.params.shortURL]
+  };
+  res.render("urls_show", templateVars);
+});
+
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
