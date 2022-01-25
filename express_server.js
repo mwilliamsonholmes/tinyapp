15adec5a2ae1d1e72b18ddf6d461be5566a80f2a
Melissa <mawilliamson10@gmail.com>
2022-01-25 01:56:47 +0000

    now requires a body-parser and created a function to generate a random string

--- express_server.js
+++ express_server.js
@@ -1,6 +1,21 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
+const bodyParser = require("body-parser");
+
+let possibilities = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+const numCharacters = 6;
+let uniqueURL = "";
+
+function generateRandomString() {
+  for (let i = 0; i < numCharacters; i++) {
+    let randomItem = Math.floor(Math.random() * possibilities.length);
+    uniqueURL += possibilities[randomItem];
+  }
+  return uniqueURL;
+}
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.set("view engine", "ejs");
 
@@ -13,10 +28,20 @@ app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);
+  res.send("Ok");
+});
+
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
+
 
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = {
