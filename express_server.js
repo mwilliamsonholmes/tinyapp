28dcda27d00cd6a5f83721e635944ac16ba69fb7
Melissa <mawilliamson10@gmail.com>
2022-01-25 04:24:06 +0000

    routes to handle shortURLs

--- express_server.js
+++ express_server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: true }));
 
 let possibilities = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
 const numCharacters = 6;
@@ -15,8 +16,6 @@ function generateRandomString() {
   return uniqueURL;
 }
 
-app.use(bodyParser.urlencoded({ extended: true }));
-
 app.set("view engine", "ejs");
 
 const urlDatabase = {
@@ -24,25 +23,43 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+//homepage
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
+//form for creating  new tiny URL
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
 
+//want to save shortURL/long URL pairs to the urlDatabase
 app.post("/urls", (req, res) => {
-  console.log(req.body);
-  res.send("Ok");
+  const newShortURL = generateRandomString();
+  urlDatabase[newShortURL] = req.body.longURL;
+  res.redirect(`/urls/${newShortURL}`);
 });
 
+//will redirect to the corresponding longURL
+app.get("/urls/:shortURL", (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL];
+  res.redirect(longURL);
+});
+
+//route to handle shortURL requests
+app.get("/u/:shortURL", (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL];
+  res.redirect(longURL);
+});
+
+
+//lists off the short URLs and their corresponding long URL in the database
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
 
-
+//shows the long URL that corresponds to the short URL inputted 
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = {
     shortURL: req.params.shortURL,
@@ -51,22 +68,23 @@ app.get("/urls/:shortURL", (req, res) => {
   res.render("urls_show", templateVars);
 });
 
+//JSON string representing urlDatabase object
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
-});
+// app.get("/hello", (req, res) => {
+//   res.send("<html><body>Hello <b>World</b></body></html>\n");
+// });
 
-app.get("/set", (req, res) => {
-  const a = 1;
-  res.send(`a = ${a}`);
-});
+// app.get("/set", (req, res) => {
+//   const a = 1;
+//   res.send(`a = ${a}`);
+// });
 
-app.get("/fetch", (req, res) => {
-  res.send(`a = ${a}`);
-})
+// app.get("/fetch", (req, res) => {
+//   res.send(`a = ${a}`);
+// })
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
