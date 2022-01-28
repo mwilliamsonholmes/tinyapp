2c7c76dc8198f948578a89fc02d1b885ad4cbede
Melissa <mawilliamson10@gmail.com>
2022-01-28 06:46:16 +0000

    removed helper functions as they are required in

--- express_server.js
+++ express_server.js
@@ -18,37 +18,7 @@ app.use(cookieSession({
   maxAge: 24 * 60 * 60 * 1000
 }));
 
-//helper functions//
-
-//returns the URLs for a specific user
-// const urlsForUser = function (id, urlDatabase) {
-//   const userUrls = {};
-//   for (const shortURL in urlDatabase) {
-//     if (urlDatabase[shortURL].userID === id) {
-//       userUrls[shortURL] = urlDatabase[shortURL];
-//     }
-//   }
-//   return userUrls;
-// };
-
-// const emailHasUser = function (email, userDatabase) {
-//   for (const user in userDatabase) {
-//     if (userDatabase[user].email === email) {
-//       return true;
-//     }
-//   }
-//   return false;
-// };
-
-// function generateRandomString() {
-//   let randomString = "";
-//   for (let i = 0; i < 6; i++) {
-//     const randomCharCode = Math.floor(Math.random() * 26 + 97);
-//     const randomChar = String.fromCharCode(randomCharCode);
-//     randomString += randomChar;
-//   }
-//   return randomString;
-// };
+//helper function//
 
 const verifyUserCookie = function (id) {
   if (users[id]) {
